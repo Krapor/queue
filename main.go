d30dbe29e279ef8facf923e2bef9b17c716737fe
Eugene <krapor2009@gmail.com>
2026-06-22 15:45:12 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
-port := os.Getenv("PORT")
-if port == "" {
-    port = "8080" // Локальный порт по умолчанию
+package main // Это ОБЯЗАТЕЛЬНО должно быть на 1-й строке
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Вот ПЕРЕД этой переменной port не должно быть никакого другого кода
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+    
+	// ... дальше ваш остальной код подключения к Redis/RabbitMQ
 }
-log.Fatal(http.ListenAndServe(":"+port, nil))
